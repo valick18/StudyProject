d3051551af52753952e08d4b16f1761ca35ab483
valick18 <valick18@gmail.com>
2022-12-07 17:45:19 +0200

    Добавьте файлы проекта.

---
+++ StudyProject/Controllers/EnterSystemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudingPlatform.Controllers
+{
+    public class EnterSystemController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string login, string password)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ StudyProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ContextEntitity();
+            //string name = db.tbInstitution.FirstOrDefault().DateCreate.ToString();
+            //ViewBag.name = name;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
